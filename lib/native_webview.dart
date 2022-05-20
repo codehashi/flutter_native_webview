eea90314e27775cedb1c2dbdd5fb6bfce42d4eea
ibiaalice <beatrizalice00@gmail.com>
2022-05-20 19:46:53 -0300

    update: flutter version

--- lib/native_webview.dart
+++ lib/native_webview.dart
@@ -4,25 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class NativeWebView extends StatefulWidget {
-  final Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers;
+  final Set<Factory<OneSequenceGestureRecognizer>>? gestureRecognizers;
 
   final String initialUrl;
-  final void Function(NativeWebViewController controller) onLoadStart;
-  final void Function(NativeWebViewController controller) onLoadStop;
-  final void Function(NativeWebViewController controller, String url, int code,
-      String message) onLoadError;
+  final void Function(NativeWebViewController controller)? onLoadStart;
+  final void Function(NativeWebViewController controller)? onLoadStop;
+  final void Function(
+    NativeWebViewController controller,
+    String url,
+    int code,
+    String message,
+  )? onLoadError;
 
-  const NativeWebView(
-      {Key key,
-      this.gestureRecognizers,
-      this.onWebViewCreated,
-      @required this.initialUrl,
-      this.onLoadStart,
-      this.onLoadStop,
-      this.onLoadError})
-      : super(key: key);
+  const NativeWebView({
+    Key? key,
+    required this.initialUrl,
+    this.gestureRecognizers,
+    this.onWebViewCreated,
+    this.onLoadStart,
+    this.onLoadStop,
+    this.onLoadError,
+  }) : super(key: key);
 
-  final void Function(NativeWebViewController webViewController)
+  final void Function(NativeWebViewController webViewController)?
       onWebViewCreated;
 
   @override
@@ -30,7 +34,7 @@ class NativeWebView extends StatefulWidget {
 }
 
 class _NativeWebViewState extends State<NativeWebView> {
-  NativeWebViewController _controller;
+  late NativeWebViewController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -60,22 +64,25 @@ class _NativeWebViewState extends State<NativeWebView> {
     return Text('$defaultTargetPlatform is not yet supported by this plugin');
   }
 
-  Future<void> onPlatformCreated(int id) async {
+  Future<void> onPlatformCreated(int? id) async {
     assert(id != null);
-    _controller = NativeWebViewController(id, widget);
+    _controller = NativeWebViewController(id: id!, webView: widget);
     if (widget.onWebViewCreated != null) {
-      widget.onWebViewCreated(_controller);
+      widget.onWebViewCreated!.call(_controller);
     }
   }
 }
 
 class NativeWebViewController {
-  MethodChannel _channel;
+  late MethodChannel _channel;
 
   final int id;
-  final NativeWebView _webView;
+  final NativeWebView? webView;
 
-  NativeWebViewController(this.id, this._webView) {
+  NativeWebViewController({
+    required this.id,
+    this.webView,
+  }) {
     _channel = MethodChannel('com.codehashi/flutter_native_webview_$id');
     _channel.setMethodCallHandler(handleMethod);
   }
@@ -83,21 +90,21 @@ class NativeWebViewController {
   Future<dynamic> handleMethod(MethodCall call) async {
     switch (call.method) {
       case "onLoadStart":
-        if (_webView != null && _webView.onLoadStart != null) {
-          _webView.onLoadStart(this);
+        if (webView != null && webView!.onLoadStart != null) {
+          webView!.onLoadStart!(this);
         }
         break;
       case "onLoadStop":
-        if (_webView != null && _webView.onLoadStop != null) {
-          _webView.onLoadStop(this);
+        if (webView != null && webView!.onLoadStop != null) {
+          webView!.onLoadStop!(this);
         }
         break;
       case "onLoadError":
         final String url = call.arguments["url"] as String;
         final int code = call.arguments["code"] as int;
         final String message = call.arguments["message"] as String;
-        if (_webView != null && _webView.onLoadError != null) {
-          _webView.onLoadError(this, url, code, message);
+        if (webView != null && webView!.onLoadError != null) {
+          webView!.onLoadError!(this, url, code, message);
         }
         break;
       default:
@@ -105,7 +112,7 @@ class NativeWebViewController {
     }
   }
 
-  Future<dynamic> evaluateJavascript({@required String source}) async {
+  Future<dynamic> evaluateJavascript({required String source}) async {
     final Map<String, dynamic> args = <String, dynamic>{};
     args.putIfAbsent('source', () => source);
     final data = await _channel.invokeMethod('evaluateJavascript', args);
